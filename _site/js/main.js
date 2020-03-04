e4ced587138230e830af2c24926f9cef6ef24d94
Jhonatas Rodrigues <jhonatasrm@gmail.com>
2020-03-04 11:07:16 -0300

    Update main.js

--- _site/js/main.js
+++ _site/js/main.js
@@ -9,7 +9,7 @@ $(document).ready(function () {
       $('.panel-cover').css('max-width', currentWidth)
       $('.panel-cover').animate({'max-width': '530px', 'width': '40%'}, 400, swing = 'swing', function () {})
     }
-  })
+  }, {once : true})
 
   if (window.location.hash && window.location.hash == '#blog') {
     $('.panel-cover').addClass('panel-cover--collapsed')
