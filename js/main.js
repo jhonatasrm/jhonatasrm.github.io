613b62b956d2d0e56d3465264287d15b9f8e98b4
Jhonatas Machado <jhonatasrm@gmail.com>
2021-11-07 21:56:51 -0300

    update website
    updated animations and added a close button to "About Me"

--- js/main.js
+++ js/main.js
@@ -5,44 +5,43 @@ sitemap:
 ---
 
 $(document).ready(function () {
-  $('a.blog-button').click(function (e) {
-    if ($('.panel-cover').hasClass('panel-cover--collapsed')) return
-    currentWidth = $('.panel-cover').width()
-    if (currentWidth < 960) {
-      //$('a.blog-button').css('visibility', 'hidden')
-      $('.panel-cover').addClass('panel-cover--collapsed')
-      $('.content-wrapper').addClass('animated slideInRight')
-    } else {
-      $('.panel-cover').css('max-width', currentWidth)
-      $('.panel-cover').animate({'max-width': '530px', 'width': '40%'}, 400, swing = 'swing', function () {})
-      $('a.blog-button').css('visibility', 'visible')
-    }
-  })
-
-  $('#about-button').click(function(){
-    $('#about-button').css('display', 'none');
-  })
-
   if(window.location.href.includes('/about')){
     $('#about-button').css('display', 'none');
   }
-  
-  if (window.location.hash && window.location.hash == '#blog') {
-    $('.panel-cover').addClass('panel-cover--collapsed')
-  }
 
+  $('#close-content').click(function(){
+    $('#about-button').css('display', 'block');
+    $('.panel-cover').removeClass('panel-cover--collapsed');
+  });
+ /* 
+  if (window.location.hash && window.location.hash == '#blog') {
+    $('.panel-cover').addClass('panel-cover--collapsed');
+  }
+ */
   if (window.location.pathname !== '{{ site.baseurl }}/' && window.location.pathname !== '{{ site.baseurl }}/index.html') {
-    $('.panel-cover').addClass('panel-cover--collapsed')
+    $('.panel-cover').addClass('panel-cover--collapsed');
   }
 
   $('.btn-mobile-menu').click(function () {
-    $('.navigation-wrapper').toggleClass('visible animated bounceInDown')
-    $('.btn-mobile-menu__icon').toggleClass('icon-list icon-x-circle animated fadeIn')
-  })
+    $('.navigation-wrapper').toggleClass('visible animated bounceInDown');
+    $('.btn-mobile-menu__icon').toggleClass('icon-list icon-x-circle animated fadeIn');
+  });
 
-  $('.navigation-wrapper .blog-button').click(function () {
-    $('.navigation-wrapper').toggleClass('visible')
-    $('.btn-mobile-menu__icon').toggleClass('icon-list icon-x-circle animated fadeIn')
-  })
+  $('.navigation-wrapper #about-button').click(function () {
+    $('.navigation-wrapper').toggleClass('visible');
+    $('.btn-mobile-menu__icon').toggleClass('icon-list icon-x-circle animated fadeIn');
+  });
+
+  $('#about-button').click(function(){
+    if ($('.panel-cover').hasClass('panel-cover--collapsed')) return
+    if (window.width < 960) {
+      $('.panel-cover').addClass('panel-cover--collapsed');
+      $('.content-wrapper').addClass('animated slideInRight');
+    } else {
+      $('.panel-cover').css('max-width', currentWidth);
+      $('.panel-cover').animate({'max-width': '530px', 'width': '40%'}, 400, swing = 'swing', function () {});
+      $('#about-button').css('visibility', 'visible');
+    }
+  });
 
 })
