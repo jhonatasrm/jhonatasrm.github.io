135933a713488f1dc0b39bf4353de7328be1a83d
Jhonatas Machado <jhonatasrm@gmail.com>
2021-11-10 00:05:44 -0300

    update website
    fixes issues with Safari
    updates social networks
    fixes minor issues

--- _site/js/main.js
+++ _site/js/main.js
@@ -1,11 +1,13 @@
 $(document).ready(function () {
   if(window.location.href.includes('/about')){
     $('#about-button').css('display', 'none');
+    $('.navigation-wrapper').css('margin-top', '-25px');
   }
 
   $('#close-content').click(function(){
     $('#about-button').css('display', 'block');
     $('.panel-cover').removeClass('panel-cover--collapsed');
+    $('.navigation-wrapper').css('margin-top', '0px');
   });
  /* 
   if (window.location.hash && window.location.hash == '#blog') {
