38db7513870f8d08282bdcfb1e4f7c7cb096530e
Jhonatas Machado <jhonatas.machado@compasso.com.br>
2022-08-19 21:13:44 -0300

    layout improvements and updated contact

--- _site/js/main.js
+++ _site/js/main.js
@@ -1,20 +1,25 @@
 $(document).ready(function () {
   if(window.location.href.includes('/about')){
-    $('#about-button').css('display', 'none');
     $('.cover-navigation').css('margin-top', '10px');
     $('#about').css("display", "none");
+    //$('#close-content').css('position', 'absolute');
+    $('#close-content').css("margin-top", "50px");
+    $('#close-content').css("display", "block");
+    $('#close-content').css("right", "0px");
+    $('#close-content').addClass('btn-mobile-close-icon icon-close-circle animated fadeInDownBig d-flex-img');
     //$('.navigation-wrapper').css('margin-top', '0px'); 
     //$('.navigation--social').css('padding-top', '25px');
-  }else{
-    $('#about').css("display", "inline-block");
+  } else {
+    $('#close-content').css("margin-top", "-5000px");
+    $('#close-content').css("display", "none");
   }
 
   $('#close-content').click(function(){
     document.title = "Jhonatas Machado";
     window.history.pushState('state', 'title', '/');
-    $('#about-button').css('display', 'block');
+    //$('#about-button').css('visibility', 'visible');
     $('.panel-cover').removeClass('panel-cover--collapsed');
-    $('#about').css("display", "inline-block");
+    //$('#about').css("display", "inline-block");
     ////margin-top', '10px'); 
     //$('.navigation--social').css('padding-top', '0px');
     //$('.navigation--social').css('margin-top', '-7px');
@@ -53,16 +58,29 @@ $(document).ready(function () {
   });
 
   $('#about-button').click(function(){
+    document.title = "About | Jhonatas Machado";
+    window.history.pushState('state', 'title', '/about');
+    $('#about-button').css('display', 'none');
+    $('#about').css('display', 'none');
+    setTimeout(function(){
+      $('#close-content').addClass('btn-mobile-close-icon icon-close-circle animated fadeInDownBig d-flex-img');
+      $('#close-content').css('position', 'absolute');
+      $('#close-content').css("margin-top", "50px");
+      $('#close-content').css("display", "block");
+      $('#close-content').css("right", "0px");
+    },500);
     if ($('.panel-cover').hasClass('panel-cover--collapsed')) return
     if (window.width < 960) {
       $('.panel-cover').addClass('panel-cover--collapsed');
-      $('.content-wrapper').addClass('animated slideInRight');
+      $('.content-wrapper').addClass('animated slideInTop');
     } else {
+      $('.panel-cover').addClass('panel-cover--collapsed');
+      $('.content-wrapper').addClass('animated slideInRight');
       $('.panel-cover').css('max-width', currentWidth);
       $('.panel-cover').animate({'max-width': '530px', 'width': '40%'}, 400, swing = 'swing', function () {});
-      $('#about-button').css('visibility', 'visible');
        if (window.innerWidth > 960) {
-          //$('#about-button').css('margin-bottom', '20px');
+        $('.panel-cover').addClass('panel-cover--collapsed');
+        $('.content-wrapper').addClass('animated slideInRight');
        }
     }
   });
@@ -70,17 +88,28 @@ $(document).ready(function () {
 })
 $(document).ready(function () {
   if(window.location.href.includes('/about')){
-    $('#about-button').css('display', 'none');
+    $('#about').css('display', 'none');
+    setTimeout(function(){
+      $('#close-content').addClass('btn-mobile-close-icon icon-close-circle animated fadeInDownBig d-flex-img');
+      $('#close-content').css('position', 'absolute');
+      $('#close-content').css("margin-top", "50px");
+      $('#close-content').css("display", "block");
+      $('#close-content').css("right", "0px");
+    },500);
     //$('.navigation-wrapper').css('margin-top', '10px'); 
 
     //$('.navigation--social').css('padding-top', '25px');
+  } else {
+    $('#about-button').css('display', 'block');
   }
 
   $('#close-content').click(function(){
     document.title = "Jhonatas Machado";
     window.history.pushState('state', 'title', '/');
     $('#about-button').css('display', 'block');
+    $('#about').css('display', 'inline-block');
     $('.panel-cover').removeClass('panel-cover--collapsed');
+    $('#close-content').removeClass('btn-mobile-close-icon icon-close-circle animated fadeInDownBig d-flex-img');
     //$('.navigation-wrapper').css('margin-top', '10px');  
     //$('.navigation--social').css('padding-top', '0px');
     //$('.navigation--social').css('margin-top', '-7px');
@@ -110,6 +139,14 @@ $(document).ready(function () {
   });
 
   $('#about-button').click(function(){
+    $('#about').css('display', 'none');
+    setTimeout(function(){
+      $('#close-content').addClass('btn-mobile-close-icon icon-close-circle animated fadeInDownBig d-flex-img');
+      $('#close-content').css('position', 'absolute');
+      $('#close-content').css("margin-top", "50px");
+      $('#close-content').css("display", "block");
+      $('#close-content').css("right", "0px");
+    },500);
     if ($('.panel-cover').hasClass('panel-cover--collapsed')) return
     if (window.width < 960) {
       $('.panel-cover').addClass('panel-cover--collapsed');
@@ -117,7 +154,7 @@ $(document).ready(function () {
     } else {
       $('.panel-cover').css('max-width', currentWidth);
       $('.panel-cover').animate({'max-width': '530px', 'width': '40%'}, 400, swing = 'swing', function () {});
-      $('#about-button').css('visibility', 'visible');
+      //$('#about-button').css('visibility', 'visible');
        if (window.innerWidth > 960) {
           //$('#about-button').css('margin-bottom', '20px');
        }
